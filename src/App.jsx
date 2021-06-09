f80ded9ad591fe07954084ab9f0f9c7417d6ad14
Rushi Arumalla <rushi@mosaic.us>
2021-06-08 20:55:55 -0700

    yeet

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,70 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from "react";
+import { AgGridColumn, AgGridReact } from "ag-grid-react";
+import "ag-grid-enterprise";
+import './styles.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-alpine.css";
+
+const fullWidth = (params) => {
+  console.log(params.api.getColumnDefs())
+  const eDiv = document.createElement("div");
+  eDiv.innerHTML = "<div class='container'>hi</div>";
+  return eDiv.firstChild
+};
+
+const columnDefs = [
+  {
+    headerName: "Type",
+    field: "type",
+    rowGroup: true,
+    hide: true,
+  },
+  {
+    headerName: "Make",
+    rowGroup: true,
+    hide: true,
+    valueGetter: ({ data }) => data?.make,
+  },
+  {
+    headerName: "Model",
+    valueGetter: ({ data }) => data?.model,
+  },
+  {
+    headerName: "Price",
+    valueGetter: ({ data }) => data?.price,
+  },
+];
+
+const App = () => {
+  const rowData = [
+    { make: "Toyota", model: "Celica", price: 35000, type: "car" },
+    { make: "Toyota", model: "Celica", price: 33000, type: "car" },
+    { make: "Toyota", type: "car", fullWidth: true },
+    { make: "Ford", model: "Mondeo", price: 32000, type: "car" },
+    { make: "Porsche", model: "Boxter", price: 72000, type: "car" },
+    { make: "Harley", model: "Davidson", price: 72000, type: "motorbike" },
+  ];
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="ag-theme-alpine" style={{ height: 400, width: 600 }}>
+      <AgGridReact
+        rowData={rowData}
+        columnDefs={columnDefs}
+        defaultColDef={{
+          flex: 1,
+          minWidth: 100,
+          filter: true,
+          sortable: true,
+          resizable: true,
+        }}
+        autoGroupColumnDef={{ minWidth: 200 }}
+        fullWidthCellRenderer={fullWidth}
+        isFullWidthCell={({ data }) => data?.fullWidth}
+        // getRowHeight={({node}) => node?.data?.fullWidth && 75 }
+      />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
